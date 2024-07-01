fce2162852e982767269602375b6715c2b76839f
Luciano Brito <lucianocassio66@gmail.com>
2024-07-01 10:40:33 -0300

    criação de calculadora aritmética

---
+++ calculadora-aritimetica/src/App.vue
@@ -0,0 +1,29 @@
+<script>
+import Calculadora from './components/Calculadora.vue';
+
+export default {
+  name: 'App',
+  components: {
+    Calculadora
+  }
+};
+</script>
+
+<template>
+
+    <Calculadora />
+
+</template>
+
+
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ calculadora-aritimetica/src/components/Calculadora.vue
@@ -0,0 +1,69 @@
+<template>
+    <div class="calculator">
+        <input type="number" v-model.number="number1" @input="calculate" placeholder="Primeiro número">
+        <select v-model="operation" @change="calculate">
+            <option value="+">+</option>
+            <option value="-">-</option>
+            <option value="*">*</option>
+            <option value="/">/</option>
+        </select>
+        <input type="number" v-model.number="number2" @input="calculate" placeholder="Segundo número">
+        <div class="result">
+            <h3>Resultado: {{ result.toFixed(3) }}</h3>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            number1: 0,
+            number2: 0,
+            operation: '+',
+            result: 0
+        };
+    },
+    methods: {
+        calculate() {
+            switch (this.operation) {
+                case '+':
+                    this.result = this.number1 + this.number2;
+                    break;
+                case '-':
+                    this.result = this.number1 - this.number2;
+                    break;
+                case '*':
+                    this.result = this.number1 * this.number2;
+                    break;
+                case '/':
+                    this.result = this.number2 !== 0 ? this.number1 / this.number2 : 'Não pode ser dividido por 0';
+                    break;
+            }
+        }
+    },
+    mounted() {
+        this.calculate();
+    }
+};
+</script>
+
+<style scoped>
+.calculator {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    margin-top: 50px;
+}
+
+input,
+select {
+    margin: 10px;
+    padding: 10px;
+    font-size: 1.2em;
+}
+
+.result {
+    margin-top: 20px;
+}
+</style>
